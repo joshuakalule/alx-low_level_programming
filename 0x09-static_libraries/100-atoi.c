605d1bbbe70847b5c32dcf37414d55f092344333
Kalule Joshua <kaljoshkalule@gmail.com>
2023-09-02 12:32:50 +0000

    add files for static libraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		if ('a' == *(s + 1))
+			return (0);
+	}
+	return (1);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+Static Libraries
+ 
+ Learn to compile and generate .o files and add them to libraries
